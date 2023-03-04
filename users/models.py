b6f301e6bac21d1ea3d030c697120318d80df52b
n-vgh98 <nazi.gh988@gmail.com>
2023-03-04 21:04:19 +0330

    Create User,EmployerProfile,EmployeeProfile in users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, null=True)
+
+
+class EmployerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class EmployeeProfile(models.Model):
+    employer = models.ForeignKey(EmployerProfile, on_delete=models.DO_NOTHING, related_name='employer_employees')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
